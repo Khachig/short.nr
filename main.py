28960508e16d6473d02ca5752ba060247c524c72
Khachig Ainteblian <ainteblian99@gmail.com>
2020-05-29 23:57:25 -0400

    add css, add form redirection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/handle_url', methods=['POST'])
+def handle_url():
+    input_url = request.form['url_input']
+    print(input_url)
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
